2be7f5a0b57e87af140a3ec5caf46b4f1d139b54
Carlsiry <chenkansheng0@163.com>
2017-10-05 15:18:15 +0800

    添加基本的拖放指令

--- src/app/directive/drag-drop/drop.directive.ts
+++ src/app/directive/drag-drop/drop.directive.ts
@@ -1,10 +1,38 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Renderer2, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appDroppable]'
 })
 export class DropDirective {
 
-  constructor() { }
+  @Input()
+  dragEnterClass: string;
+
+  // 注入 Dom引用、Dom渲染处理
+  constructor(private el: ElementRef, private rd: Renderer2) { }
+
+  @HostListener('dragenter', ['$event'])
+  onDragEnter(ev: Event) {
+    if (this.el.nativeElement === ev.target) {
+      this.rd.addClass(this.el.nativeElement, this.dragEnterClass);
+    }
+  }
+  @HostListener('dragover', ['$event'])
+  onDragOver(ev: Event) {
+    if (this.el.nativeElement === ev.target) {
+    }
+  }
+  @HostListener('dragleave', ['$event'])
+  onDragLeave(ev: Event) {
+    if (this.el.nativeElement === ev.target) {
+      this.rd.removeClass(this.el.nativeElement, this.dragEnterClass);
+    }
+  }
+  @HostListener('drop', ['$event'])
+  onDrag(ev: Event) {
+    if (this.el.nativeElement === ev.target) {
+      this.rd.removeClass(this.el.nativeElement, this.dragEnterClass);
+    }
+  }
 
 }
