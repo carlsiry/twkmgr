81f3fd7c699dbe2e01549b74ea9d8071be8d4957
Carlsiry <chenkansheng0@163.com>
2017-10-05 18:52:57 +0800

    改造了一下登录表单

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
 
 const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
@@ -10,10 +10,19 @@ const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA
 })
 export class LoginComponent implements OnInit {
 
-  userFormControl = new FormControl('', [Validators.required, Validators.pattern(EMAIL_REGEX)]);
+  // 自定义表单
+  loginForm = new FormGroup({
+    email: new FormControl('', Validators.compose([Validators.required, Validators.email])),
+    password: new FormControl('', [Validators.required])
+  });
+
   constructor() { }
 
   ngOnInit() {
   }
-
+  // 自己处理表单提交事件
+  onSubmit({value, valid}, ev: Event) {
+    console.log(value);
+    console.log(valid);
+  }
 }
