ab41330fff4d9488b991c944489cdb78429fc89b
Carlsiry <chenkansheng0@163.com>
2017-10-09 18:47:16 +0800

    添加跟项目相关的用户服务

--- src/app/services/services.module.ts
+++ src/app/services/services.module.ts
@@ -2,6 +2,8 @@
 /**
  * 2017.10.08 创建服务模块，用于提供整个项目的接口服务
  *  - 提供 项目接口服务
+ * 2017.10.09 下午 逸夫楼 -- Carlsiry 
+ *  - 添加用户服务，提供跟用户相关的项目增删改查
  */
 
 import { NgModule } from '@angular/core';
@@ -9,12 +11,14 @@ import { CommonModule } from '@angular/common';
 import { ProjectService } from './project.service';
 import { TaskListService } from './task-list.service';
 import { TaskService } from './task.service';
+import { UserService } from './user.service';
 
 @NgModule({
   providers: [
     ProjectService,
     TaskListService,
     TaskService,
+    UserService,
   ]
 })
 export class ServicesModule { }
